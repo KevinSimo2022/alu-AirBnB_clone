6fe2e517fe8d180d997cbea94349e76d650f5df0
KevinSimo2022 <116545828+KevinSimo2022@users.noreply.github.com>
2023-09-10 12:19:07 +0200

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -15,6 +15,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            self.__class__ = BaseModel
 
     def __str__(self):
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
