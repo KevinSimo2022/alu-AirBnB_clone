0749619f87153c4a6cc38b42bf11178bb5cc0f6d
KevinSimo2022 <116545828+KevinSimo2022@users.noreply.github.com>
2023-09-10 13:02:33 +0200

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,30 +1,67 @@
+#!/usr/bin/python3
+""" this is the base class for all models """
 import uuid
-from datetime import datetime
+from datetime import date, datetime, time
+from models import storage
 
 
 class BaseModel:
-    def __init__(self, *args, **kwargs):
+    """class defining commom attributes
+    for other classes"""
+
+    def __init__(self, *argv, **kwargs):
+        """ instance attributes
+        id : string
+        created_at: datetime
+        updated_at: datetime
+
+        getting arguments to recreate
+        BaseModel
+        creates new instance Model
+        """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+
         if kwargs:
             for key, value in kwargs.items():
-                if key == 'created_at' or key == 'updated_at':
-                    setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
-                else:
-                    setattr(self, key, value)
+                if key != '__class__':
+                    if key in ['created_at', 'updated_at']:
+                        temp_dict = self.__dict__
+                        temp_dict[key] = \
+                            datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    else:
+                        temp_dict = self.__dict__
+                        temp_dict[key] = value
+
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            self.__class__ = BaseModel  # Set __class__ to the actual class of the object
+            self.updated_at = self.created_at
+            storage.new(self)
 
     def __str__(self):
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        """ default string output of class name \
+                ,id and dictionary files """
+        return '[{}] ({}) {}'\
+            .format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
+        """
+        update of attribute updated_at
+        with latest time
+        updated_at: datetime
+        """
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
-        obj_dict = self.__dict__.copy()
-        obj_dict['__class__'] = self.__class__.__name__
-        obj_dict['created_at'] = self.created_at.isoformat()
-        obj_dict['updated_at'] = self.updated_at.isoformat()
-        return obj_dict
+        """
+        get the objects of the instance
+        returns: dictionary
+        """
+        temp_dict = self.__dict__.copy()
+        temp_dict['__class__'] = self.__class__.__name__
+        temp_dict['created_at'] = temp_dict['created_at'].isoformat()
+        temp_dict['updated_at'] = temp_dict['updated_at'].isoformat()
+        return temp_dict
