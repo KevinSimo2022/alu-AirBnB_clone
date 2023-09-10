1b3c914bf98ba49aff25fd2e3eaef78a234d2ccd
KevinSimo2022 <k.takoulo@alustudent.com>
2023-09-10 10:22:29 +0200

    done

--- models/base_model.py
+++ models/base_model.py
@@ -7,8 +7,7 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         if kwargs:
             for key, value in kwargs.items():
-                if key == 'created_at' or 
-                                     key == 'updated_at':
+                if key == 'created_at' or key == 'updated_at':
                     setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
                 else:
                     setattr(self, key, value)
--- models/city.py
+++ models/city.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """attributes of the city class"""
 from models.base_model import BaseModel
+
+
 class City(BaseModel):
     state_id = ""
     name = ""
