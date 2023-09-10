37ea7d8d48f4ea1eec1398e3cabc302c65178e55
KevinSimo2022 <k.takoulo@alustudent.com>
2023-09-10 10:34:34 +0200

    d

--- models/base_model.py
+++ models/base_model.py
@@ -7,7 +7,8 @@ class BaseModel:
         if kwargs:
             for key, value in kwargs.items():
                 if key == 'created_at' or key == 'updated_at':
-                    setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                    setattr(self, key, datetime.strptime(value,
+                        "%Y-%m-%dT%H:%M:%S.%f"))
                 else:
                     setattr(self, key, value)
         else:
@@ -16,7 +17,8 @@ class BaseModel:
             self.updated_at = datetime.now()
 
     def __str__(self):
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                self.id, self.__dict__)
 
     def save(self):
         self.updated_at = datetime.now()
@@ -27,4 +29,3 @@ class BaseModel:
         obj_dict['created_at'] = self.created_at.isoformat()
         obj_dict['updated_at'] = self.updated_at.isoformat()
         return obj_dict
-
