df67ccc266f2f7f913927c4eaafba8e048927131
KevinSimo2022 <116545828+KevinSimo2022@users.noreply.github.com>
2023-09-11 10:33:45 +0200

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,43 +1,39 @@
-#!/usr/bin/python3
-"""Defines FileStorage class."""
+import os
 import json
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
 
 
 class FileStorage:
-    __file_path = 'file.json'
+    __file_path = "file.json"
     __objects = {}
-
     def all(self):
-        """Return dictionary __objects."""
+        """Return the dictionary of objects."""
         return self.__objects
-
     def new(self, obj):
-        """Set in __objects obj with the  key <obj_class_name>.id"""
-        key = '{}.{}'.format(obj.__class__.__name__, obj.id)
+        """Add an object to the current database."""
+        key = f"{obj.__class__.__name__}.{obj.id}"
         self.__objects[key] = obj
-
     def save(self):
-        """Serialize __objects to JSON file __file_path."""
-        object_dict = {}
-        for obj in self.__objects:
-            object_dict[obj] = self.__objects[obj].to_dict()
-        with open(self.__file_path, 'w') as file:
-            json.dump(object_dict, file)
-
+        """Serialize __objects to JSON file."""
+        new_dict = {}
+        for key, obj in self.__objects.items():
+            new_dict[key] = obj.to_dict()
+        with open(self.__file_path, mode="w", encoding="UTF-8") as file:
+            json.dump(new_dict, file)
     def reload(self):
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.state import State
-        from models.city import City
-        from models.place import Place
-        from models.amenity import Amenity
-        from models.review import Review
-
+        """Deserialize JSON file to __objects."""
         try:
-            with open(self.__file_path) as file:
-                serialized_content = json.load(file)
-                for item in serialized_content.values():
-                    class_name = item['__class__']
-                    self.new(eval(class_name + "(**" + str(item) + ")"))
+            with open(self.__file_path, mode="r", encoding="UTF-8") as f:
+                new_obj_dict = json.load(f)
+                for key, value in new_obj_dict.items():
+                    class_name = value['__class__']
+                    new_instance = eval(class_name)(**value)
+                    self.__objects[key] = new_instance
         except FileNotFoundError:
             pass
