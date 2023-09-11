b0ac0f66f760071bca89c0a8c318193f0ab3aed6
KevinSimo2022 <116545828+KevinSimo2022@users.noreply.github.com>
2023-09-11 09:40:52 +0200

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,42 +1,77 @@
 #!/usr/bin/python3
-import uuid
+"""Defines BaseModel class."""
+import models
+from uuid import uuid4
 from datetime import datetime
 
+
 class BaseModel:
+    """
+    Base Model Class
+
+    This class defines common attributes and methods for other classes.
+
+    Attributes:
+        id (str): A unique identifier generated with UUID.
+        created_at (datetime): The datetime when the instance is created.
+        updated_at (datetime): The datetime of the last update to the instance.
+    """
+
     def __init__(self, *args, **kwargs):
-        if kwargs:
-            for key, value in kwargs.items():
-                if key == 'created_at' or key == 'updated_at':
-                    setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
-                else:
-                    setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+        """
+        Initialize a new BaseModel instance.
 
-    def __str__(self):
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        Args:
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments.
+        """
 
-    def save(self):
+        datetime_format = "%Y-%m-%dT%H:%M:%S.%f"
+
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
-    def to_dict(self):
-        obj_dict = self.__dict__.copy()
-        obj_dict['__class__'] = self.__class__.__name__
-        obj_dict['created_at'] = self.created_at.isoformat()
-        obj_dict['updated_at'] = self.updated_at.isoformat()
-        return obj_dict
+        if kwargs:
+            kwargs["created_at"] = datetime.strptime(
+                kwargs["created_at"], datetime_format)
+            kwargs["updated_at"] = datetime.strptime(
+                kwargs["updated_at"], datetime_format)
+            del kwargs["__class__"]
+            self.__dict__.update(kwargs)
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
 
-    @classmethod
-    def from_dict(cls, obj_dict):
-        if '__class__' in obj_dict:
-            class_name = obj_dict.pop('__class__')
-            if class_name == cls.__name__:
-                obj_dict['created_at'] = datetime.fromisoformat(obj_dict['created_at'])
-                obj_dict['updated_at'] = datetime.fromisoformat(obj_dict['updated_at'])
-                instance = cls()
-                for key, value in obj_dict.items():
-                    setattr(instance, key, value)
-                return instance
-        raise ValueError("Invalid or mismatched class name in dictionary representation")
+    def save(self):
+        """
+        Update the `updated_at` attribute with the current datetime
+        and save the changes.
+        """
+        self.updated_at = datetime.now()
+        models.storage.save()
+
+    def to_dict(self):
+        """
+        Return a dictionary representation of the BaseModel instance.
+
+        Returns:
+            dict: A dictionary containing the object's attributes.
+        """
+        result_dict = self.__dict__.copy()
+        result_dict["created_at"] = self.created_at.isoformat()
+        result_dict["updated_at"] = self.updated_at.isoformat()
+        result_dict["__class__"] = self.__class__.__name__
+        return result_dict
+
+    def __str__(self):
+        """
+        Return a string representation of the BaseModel instance.
+
+        Returns:
+            str: A formatted string containing class name, ID, and attributes.
+        """
+        class_name = self.__class__.__name__
+        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
