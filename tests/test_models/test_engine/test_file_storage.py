0d38355d67cb2f1df680fb676daffadf39165ad0
KevinSimo2022 <116545828+KevinSimo2022@users.noreply.github.com>
2023-09-12 13:34:59 +0200

    Update test_file_storage.py

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,44 +1,93 @@
 #!/usr/bin/python3
-import os
+""" Contains unittests for FileStorage class """
 import unittest
-from os import path
+import os
+from models import storage
 from models.base_model import BaseModel
-from models.engine.file_storage import FileStorage
-""" testing the file storage"""
 
 
-class TestCaseFileStorage(unittest.TestCase):
-    """ class for test cases """
-
-    def setUp(self):
-        """ setting up the various
-            components for the test """
-        self.dir_path = 'file.json'
-        self.my_model = FileStorage()
-
-    def tearDown(self):
-        """ dispose json file """
-        if path.exists(self.dir_path):
-            os.remove(self.dir_path)
+class TestFileStorageClass(unittest.TestCase):
+    """ Tests FileStorage class """
 
     def test_all(self):
-        """ check type return by all function """
-        self.assertEqual(type(self.my_model.all()), dict)
+        """ Tests all method """
+        # create storage instance and instance of BaseModel
+
+        obj = BaseModel()
+        __objects = storage.all()
+        # test that storage.all() returns dictionary
+        self.assertIsInstance(__objects, dict)
+        # test that BaseModel instance in dictionary of objects
+        key = "{}.{}".format(type(obj).__name__, obj.id)
+        self.assertIn(key, __objects)
+        # test that the value in dictionary is of same type and equal to obj
+        self.assertEqual(obj, __objects[key])
+        self.assertIsInstance(__objects[key], BaseModel)
 
     def test_new(self):
-        test_model = BaseModel()
-        self.my_model.new(test_model)
-        len_dict = len(self.my_model.all())
-        self.assertGreater(len_dict, 0)
+        """ Tests new method """
+        # create storage instance and instance of BaseModel
+        obj = BaseModel()
+        # create dictionary to set obj values to with **kwargs
+        new_dict = {}
+        new_dict["id"] = "012345"
+        new_dict["created_at"] = "1995-2-2T10:23:35.123450"
+        new_dict["updated_at"] = "1999-1-4T7:15:05.543210"
+        # create instance with **kwargs and test that object is not in __objects
+        obj2 = BaseModel(**new_dict)
+        key = "{}.{}".format(type(obj2).__name__, obj2.id)
+        self.assertNotIn(key, storage.all())
+        # obj2 is reset to new instance of BaseModel and
+        # should be added to dictionary with new
+        obj2 = BaseModel()
+        key = "{}.{}".format(type(obj2).__name__, obj2.id)
+        __objects = storage.all()
+        self.assertIn(key, __objects)
+        self.assertEqual(obj2, __objects[key])
 
     def test_save(self):
-        """ save content to file
-         and create if not exist"""
-        self.my_model.save()
-        self.assertEqual(path.exists(self.dir_path), True)
+        """ Tests save method """
+        # storage = FileStorage()
+        file = "file.json"
+        # Remove file if it exists
+        if os.path.exists(file):
+            os.remove(file)
+        # Test if save creates the file
+        storage.save()
+        self.assertTrue(os.path.exists(file))
+        # Overwrite created file
+        with open(file, 'w') as f:
+            f.write("Placeholder")
+        # Check if save overwrites existing files
+        with open(file, 'r') as f:
+            content = f.read()
+            storage.save()
+            new_content = f.read()
+        self.assertNotEqual(content, new_content)
 
     def test_reload(self):
-        model = FileStorage()
-        self.my_model.reload()
-        len_dict = len(model.all())
-        self.assertGreater(len_dict, 0)
+        """ Tests reload method """
+
+        from json import dumps
+
+        # Check if reloading without doing anything reloads the same thing
+        old_dict = storage.all()
+        storage.reload()
+        new_dict = storage.all()
+        self.assertEqual(old_dict.keys(), new_dict.keys())
+
+        # Make new object and a dictionary for that object
+        obj = BaseModel()
+        obj.id = 1
+        dict_dict = {"BaseModel.1": obj.to_dict()}
+        obje_dict = {"BaseModel.1": obj}
+
+        # Overwrite file.json so that it includes just this dictionary
+        if os.path.exists("file.json"):
+            os.remove("file.json")
+        with open("file.json", 'w') as f:
+            f.write(dumps(dict_dict))
+
+        # Reload
+        storage.reload()
+        self.assertNotEqual(storage.all().keys(), obje_dict.keys())
