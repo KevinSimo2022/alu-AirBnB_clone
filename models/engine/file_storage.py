ba14149275ff4977adf009724a9ed111a5066ca1
KevinSimo2022 <k.takoulo@alustudent.com>
2023-09-12 13:11:04 +0200

    tt

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,13 +2,13 @@
 
 import json
 from os.path import exists
-from ..base_model import BaseModel
-from ..amenity import Amenity
-from ..city import City
-from ..place import Place
-from ..review import Review
-from ..state import State
-from ..user import User
+from base_model import BaseModel
+from amenity import Amenity
+from city import City
+from place import Place
+from review import Review
+from state import State
+from user import User
 
 classes = {
     "Amenity": Amenity,
