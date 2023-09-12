49b0122a37ec9d5eed7a9a3f70b25725846c3770
KevinSimo2022 <k.takoulo@alustudent.com>
2023-09-12 13:30:29 +0200

    ,jsh

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,11 +5,6 @@ from os.path import exists
 
 
 class FileStorage:
-<<<<<<< HEAD
-        """ file storage class"""
-
-=======
->>>>>>> 11d73330625703ad328d10093108563625181f3f
     __file_path = "file.json"
     __objects = {}
 
@@ -33,37 +28,6 @@ class FileStorage:
             json.dump(serialized_objects, file)
 
     def reload(self):
-<<<<<<< HEAD
-        """ deserializes JSON from the file """
-    from models.base_model import BaseModel
-    from models.user import User
-    from models.city import City
-    from models.state import State
-    from models.place import Place
-    from models.review import Review
-    from models.amenity import Amenity
-    
-    class_mapping = {
-        'BaseModel': BaseModel,
-        'User': User,
-        'City': City,
-        'State': State,
-        'Place': Place,
-        'Review': Review,
-        'Amenity': Amenity
-    }
-    
-    if path.exists(self.__file_path):
-        with open(self.__file_path, "r", encoding='utf-8') as in_file:
-            dataset = json.load(in_file)
-            for data in dataset.values():
-                class_name = data.get('__class__')
-                if class_name in class_mapping:
-                    class_instance = class_mapping[class_name](**data)
-                    self.new(class_instance)
-                else:
-                    print(f"Warning: Unknown class '{class_name}' in JSON data.")
-=======
         """
         Deserializes the JSON file to __objects
         (only if the JSON file (__file_path) exists;
@@ -74,8 +38,7 @@ class FileStorage:
             return
 
         from .class_objects import classes
-        
-        
+
         with open(FileStorage.__file_path, 'r') as file:
             data = json.load(file)
             for key, obj_data in data.items():
@@ -83,4 +46,3 @@ class FileStorage:
                 obj_class = classes[class_name]
                 obj_instance = obj_class(**obj_data)
                 FileStorage.__objects[key] = obj_instance
->>>>>>> 11d73330625703ad328d10093108563625181f3f
